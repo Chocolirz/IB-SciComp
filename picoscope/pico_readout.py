361d7b6dd351a867772e9c98a95a45fa0a4812ca
Chocolirz <rl737@cam.ac.uk>
2024-11-10 23:38:28 +0000

    added picoscope readout programme, hope this works next week

---
+++ picoscope/pico_readout.py
@@ -0,0 +1,22 @@
+from picosdk.discover import find_unit
+from picosdk.device import ChannelConfig, TimebaseOptions
+
+__all__ = ['PicoReadout']
+
+class PicoReadout:
+    def __init__(self, ADecouple, BDecouple, ARange, BRange, min_collection_time) -> None:
+        self.ADecouple = ADecouple
+        self.BDecouple = BDecouple
+        self.ARange = ARange
+        self.BRange = BRange
+        self.min_collection_time = min_collection_time
+
+    def run(self, ADecouple, BDecouple, ARange, BRange, min_collection_time):
+        with find_unit() as device:
+            channel_configs = [ChannelConfig('A', True, ADecouple, ARange), ChannelConfig('B', True, BDecouple, BRange)]
+            timebase_options = TimebaseOptions(min_collection_time)
+            times, voltages, overflow_warnings = device.capture_block(timebase_options, channel_configs)
+            return times, voltages, overflow_warnings
+
+    def __str__(self) -> str:
+        return f'Picoscope Setting: ADecouple={self.ADecouple}, BDecouple={self.BDecouple}, ARange={self.ARange}, BRange={self.BRange}, min_collection_time={self.min_collection_time}'
